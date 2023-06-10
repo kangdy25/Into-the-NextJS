4a7f10f98fcdd10a30621770c6c858a8fb333731
Kangdy <canfrog@naver.com>
2023-06-10 14:28:16 +0900

    Like Button

--- 00_nextjs_theory/app/list/page.js
+++ 00_nextjs_theory/app/list/page.js
@@ -1,6 +1,10 @@
+'use client'
+
+import { useState } from "react"
+
 export default function List() {
     let item = ['tomato', 'pasta', 'coconut']
-
+    let [count, setCount] = useState([0, 0, 0]);
     return (
         <div>
             <h4 className="title">상품 목록</h4>
@@ -10,6 +14,18 @@ export default function List() {
                         <div className="food" key={i}>
                             <img src={`/food${i}.png`} className="food-img"/>
                             <h4>{item[i]} $40</h4>
+                            <span> {count[i]} </span>
+                            <button onClick={()=>{
+                                let copyCount = [...count]
+                                copyCount[i]++
+                                setCount(copyCount)
+                            }}>+</button>
+                            <span> </span>
+                            <button onClick={()=>{
+                                let copyCount = [...count]
+                                copyCount[i]--
+                                setCount(copyCount)
+                            }}>-</button>
                         </div>
                     )
                 })
