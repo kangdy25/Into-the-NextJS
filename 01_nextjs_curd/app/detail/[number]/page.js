187f578e10bd72676a2b69e8b34285d37ca55240
Kangdy <canfrog@naver.com>
2023-07-01 18:50:54 +0900

    01 Project - Error.js, Loading.js, Not-Found.js

--- 01_nextjs_curd/app/detail/[number]/page.js
+++ 01_nextjs_curd/app/detail/[number]/page.js
@@ -1,11 +1,17 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 import Comment from "./comment";
+import { notFound } from "next/navigation";
 
 export default async function Detail(props) {
     let client = await connectDB;
     const db = client.db('forum');
     let result = await db.collection('post').findOne({_id: new ObjectId(props.params.number) })
+    
+    if (result == null) {
+        return notFound()
+    }
+
     return (
         <div>
             <h4>상세페이지</h4>
