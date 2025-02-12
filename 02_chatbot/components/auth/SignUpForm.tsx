cc98a99a5888741a0972bb9af916328f2d834224
Kangdy <canfrog@naver.com>
2025-02-12 12:53:24 +0900

    회원가입 유효성 검증

--- 02_chatbot/components/auth/SignUpForm.tsx
+++ 02_chatbot/components/auth/SignUpForm.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent } from "react";
 import FormCard from "./FormCard";
 import { Label } from "@/components/ui/label";
 import { Input } from "../ui/input";
 import Submit from "./Submit";
+import { TSignUpFormError } from "@/types/form";
+import { SignUpSchema } from "@/schemas/auth";
+
+import { useFormValidate } from "@/hooks/useFormValidate";
+import FormMessage from "./FormMessage";
+
+const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const { name, value } = event.target;
+};
 
 const SignUpForm = () => {
+  const { errors, validateField } =
+    useFormValidate<TSignUpFormError>(SignUpSchema);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    validateField(name, value);
+  };
+  console.log("errors", errors);
+
   return (
     <FormCard
       title="회원가입"
@@ -13,7 +31,14 @@ const SignUpForm = () => {
       <form className="space-y-6">
         <div className="space-y-1">
           <Label htmlFor="name">이름</Label>
-          <Input id="name" name="name" placeholder="이름을 입력해주세요" />
+          <Input
+            id="name"
+            name="name"
+            placeholder="이름을 입력해주세요"
+            onChange={handleChange}
+            error={!!errors?.name}
+          />
+          {errors?.name && <FormMessage message={errors?.name[0]} />}
         </div>
         <div className="space-y-1">
           <Label htmlFor="email">이메알</Label>
@@ -22,7 +47,10 @@ const SignUpForm = () => {
             name="email"
             type="email"
             placeholder="example@example.com"
+            onChange={handleChange}
+            error={!!errors?.name}
           />
+          {errors?.email && <FormMessage message={errors?.email[0]} />}
         </div>
         <div className="space-y-1">
           <Label htmlFor="password">비밀번호</Label>
@@ -31,7 +59,10 @@ const SignUpForm = () => {
             name="password"
             type="password"
             placeholder="**********"
+            onChange={handleChange}
+            error={!!errors?.name}
           />
+          {errors?.password && <FormMessage message={errors?.password[0]} />}
         </div>
         <Submit className="w-full">가입하기</Submit>
       </form>
