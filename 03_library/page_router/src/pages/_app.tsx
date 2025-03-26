b4cd5f48b63707219e663c876351810bc879a03a
Shineast <canfrog@naver.com>
2025-03-26 11:24:42 +0900

    Prefetching

--- 03_library/page_router/src/pages/_app.tsx
+++ 03_library/page_router/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -9,12 +10,20 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push("/test");
   };
 
+  // 프로그래메틱하게 작성된 라우팅 방식을 프리페칭하는 방법
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>
         <Link href={"/"}>Index</Link>
         &nbsp;
-        <Link href={"/search"}>Search</Link>
+        {/* Link 컴포넌트의 프리페칭을 강제적으로 해제하는 방법 */}
+        <Link href={"/search"} prefetch={false}>
+          Search
+        </Link>
         &nbsp;
         <Link href={"/book/1"}>Book/1</Link>
         <div>
