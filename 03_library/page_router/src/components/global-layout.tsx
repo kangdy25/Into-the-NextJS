700724be4bcaae46e7930fff9f21124614a4a72c
Shineast <canfrog@naver.com>
2025-03-27 12:13:45 +0900

    Setting global layout

---
+++ 03_library/page_router/src/components/global-layout.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React, { ReactNode } from "react";
+import style from "./global-layout.module.css";
+
+const GlobalLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+      </header>
+      <main className={style.main}>{children}</main>
+      <footer className={style.footer}>제작 @Swkinah</footer>
+    </div>
+  );
+};
+
+export default GlobalLayout;
--- 03_library/page_router/src/pages/_app.tsx
+++ 03_library/page_router/src/pages/_app.tsx
@@ -1,36 +1,11 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const onClickButton = () => {
-    router.push("/test");
-  };
-
-  // 프로그래메틱하게 작성된 라우팅 방식을 프리페칭하는 방법
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={"/"}>Index</Link>
-        &nbsp;
-        {/* Link 컴포넌트의 프리페칭을 강제적으로 해제하는 방법 */}
-        <Link href={"/search"} prefetch={false}>
-          Search
-        </Link>
-        &nbsp;
-        <Link href={"/book/1"}>Book/1</Link>
-        <div>
-          <button onClick={onClickButton}>test 페이지로 이동</button>
-        </div>
-      </header>
-      <Component {...pageProps} />;
-    </>
+    <GlobalLayout>
+      <Component {...pageProps} />
+    </GlobalLayout>
   );
 }
