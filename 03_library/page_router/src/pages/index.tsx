f8740fabeb0840377567ba0bf54d70ce30fb8a05
Shineast <canfrog@naver.com>
2025-03-31 15:51:49 +0900

    Introduction of Server Side Rendering

--- 03_library/page_router/src/pages/index.tsx
+++ 03_library/page_router/src/pages/index.tsx
@@ -1,10 +1,30 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어, 컴포넌트에 필요한 데이터를 불러오는 함수
+  console.log("서버 사이드 프롭스에요");
+  const data = "hello";
+
+  return {
+    props: { data },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+
+  // 서버 측에서 작동하지 않고 클라이언트에서만 작동되도록 하는 방법
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
