0efdd9ba6a8cdaa000659a00a51fd2fd707ec8a1
Shineast <canfrog@naver.com>
2025-04-05 22:35:51 +0900

    Navigation in App Router

--- 03_library/app_router/src/app/(with-searchbar)/searchBar.tsx
+++ 03_library/app_router/src/app/(with-searchbar)/searchBar.tsx
@@ -1,17 +1,28 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const SearchBar = () => {
+  const router = useRouter();
   const [search, setSearch] = useState("");
+
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") onSubmit();
+  };
+
   return (
     <div>
-      <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <input value={search} onKeyDown={onKeydown} onChange={onChangeSearch} />
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 };
