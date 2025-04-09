0fa5123e2a4c0b17f7c535e6ffba114c840b8960
Shineast <canfrog@naver.com>
2025-04-09 13:03:46 +0900

    Applying Pull Route Cache

--- 03_library/app_router/src/app/book/[id]/page.tsx
+++ 03_library/app_router/src/app/book/[id]/page.tsx
@@ -1,4 +1,11 @@
+import { notFound } from "next/navigation";
 import style from "./page.module.css";
+
+// export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }];
+}
 
 export default async function Page({
   params,
@@ -6,10 +13,12 @@ export default async function Page({
   params: Promise<{ id: string | string[] }>;
 }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${(await params).id}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${(await params).id}`,
+    { cache: "force-cache" }
   );
 
   if (!response.ok) {
+    if (response.status === 404) notFound();
     return <div>오류가 발생함 ㅠㅠ</div>;
   }
   const book = await response.json();
