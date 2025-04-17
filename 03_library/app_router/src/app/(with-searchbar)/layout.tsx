b085c33af6bdb4cfe59a899341a162f937fe7fdc
Shineast <canfrog@naver.com>
2025-04-18 01:03:53 +0900

    03 Project - done

--- 03_library/app_router/src/app/(with-searchbar)/layout.tsx
+++ 03_library/app_router/src/app/(with-searchbar)/layout.tsx
@@ -4,7 +4,6 @@ import Searchbar from "../../components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <div>{new Date().toLocaleString()}</div>
       <Suspense fallback={<div>Loading ...</div>}>
         <Searchbar />
       </Suspense>
